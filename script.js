252f9b6368f7e60312d857abe8f6c69bc3c5bd9e
Connor Todd <cgtodd85@gmail.com>
2021-06-25 16:29:34 -0400

    updated js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 const apiKey = "EU4ZrfrUY4zH5RQgUyevVAiRfS9TkLA7";
+const getWeatherdetails = async (id) => {
+  const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const apiQuery = `${id}?apikey=${apiKey}`;
+};
