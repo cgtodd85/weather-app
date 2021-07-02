4a88d8f012ab60aefffddcca1bf8dd01dc061b0a
Connor Todd <cgtodd85@gmail.com>
2021-07-02 18:55:28 -0400

    working api call

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const apiKey = "EU4ZrfrUY4zH5RQgUyevVAiRfS9TkLA7";
-const getWeatherdetails = async (id) => {
-  const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
-  const apiQuery = `${id}?apikey=${apiKey}`;
-};
+const apiKey = "74974760a33b1dee728fdbda7a227882";
+let userInput = document.querySelector("#searchbar");
+const searchBtn = document.querySelector("#search-btn");
+let results = document.querySelector("#results");
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getCityWeather();
+});
+
+function getCityWeather() {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${userInput.value}&appid=${apiKey}`
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data.main);
+      let feelsLike = ((data.main.feels_like - 273) * 9) / 5 + 32;
+      console.log(
+        "It feels like " + Math.round(feelsLike) + " degrees fahrenheit"
+      );
+    });
+}
