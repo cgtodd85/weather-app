9aedeb6bf65fde6eaaee61b660316e6682b915fd
Connor Todd <cgtodd85@gmail.com>
2021-07-07 03:23:45 -0400

    created 5 day array and fetched data from onecall api

--- script.js
+++ script.js
@@ -2,6 +2,38 @@ const apiKey = "74974760a33b1dee728fdbda7a227882";
 let userInput = document.querySelector("#searchbar");
 const searchBtn = document.querySelector("#search-btn");
 let results = document.querySelector("#results");
+let fiveDay = [
+  {
+    date: "",
+    icon: "",
+    temp: "",
+    hum: "",
+  },
+  {
+    date: "",
+    icon: "",
+    temp: "",
+    hum: "",
+  },
+  {
+    date: "",
+    icon: "",
+    temp: "",
+    hum: "",
+  },
+  {
+    date: "",
+    icon: "",
+    temp: "",
+    hum: "",
+  },
+  {
+    date: "",
+    icon: "",
+    temp: "",
+    hum: "",
+  },
+];
 
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -22,10 +54,11 @@ function getCityWeather() {
       return res.json();
     })
     .then(function (data) {
-      console.log(data);
-      console.log(data.main);
-      console.log(data.coord.lon);
+      console.log("This is the city weather fetch: ", data);
       let lon = data.coord.lon;
+      let lat = data.coord.lat;
+      oneCallApi(lat, lon);
+
       let feelsLike = Math.round(((data.main.feels_like - 273) * 9) / 5 + 32);
       let temp = Math.round(((data.main.temp - 273) * 9) / 5 + 32);
       let humidity = data.main.humidity;
@@ -45,11 +78,32 @@ function getCityWeather() {
     });
 }
 
+function oneCallApi(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log("This is the one call data: ", data);
+      for (i = 0; i < fiveDay.length; i++) {
+        fiveDay[i].date = data.daily[i].dt;
+        fiveDay[i].icon = data.daily[i].weather[0].icon;
+        fiveDay[i].temp = data.daily[i].temp.day;
+        fiveDay[i].hum = data.daily[i].humidity;
+      }
+      console.log("Five Day Array: ", fiveDay);
+    });
+}
 /*
 city name
 date
 icon for weather conditions
 5 day each with temp, conditions, humidity
+
+fiveDay
+
 search history select prev searched cities
 
 how to protect apikey?
